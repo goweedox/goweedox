4beeddae7b6509ac9163bfafbd664dec55c00603
wella <loue_vill@yahoo.com>
2010-06-03 01:54:56 -0700

    spaces and tabs fixed...

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,25 +12,25 @@ class WelcomeController < ApplicationController
 
   def login
     if session[:user_id] 
-	    redirect_to(:controller => "admin", :action => "index")
+      redirect_to(:controller => "admin", :action => "index")
     end  
-     if request.post?
-       user = User.authenticate(params[:name], params[:password])   
-       if user &&  (user.name == 'edsonpaul' || user.name =='rj' || user.name =='wella' || user.name =='elodez'|| user.name =='jose' || user.name =='harmy')
-         session[:user_id] = user.id
-         redirect_to(:controller => 'admin', :action => "index")
-       elsif user
-	       session[:user_id] = user.id
-         redirect_to(:controller => "limited", :action => "index")
-       else
-         flash.now[:notice] = "Invalid user/password combination!"
-       end
-     end
+    if request.post?
+      user = User.authenticate(params[:name], params[:password])   
+      if user &&  (user.name == 'edsonpaul' || user.name =='rj' || user.name =='wella' || user.name =='elodez'|| user.name =='jose' || user.name =='harmy')
+        session[:user_id] = user.id
+        redirect_to(:controller => 'admin', :action => "index")
+      elsif user
+	session[:user_id] = user.id
+        redirect_to(:controller => "limited", :action => "index")
+      else
+        flash.now[:notice] = "Invalid user/password combination!"
+      end
+    end
   end
   
   def index
-	  if session[:user_id] 
-	    redirect_to(:controller => "admin", :action => "index")
-	  end
+    if session[:user_id] 
+      redirect_to(:controller => "admin", :action => "index")
+    end
   end
 end
